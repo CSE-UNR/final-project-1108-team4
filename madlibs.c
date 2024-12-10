99650c6056fdc3e1fb730100d139ac2afda470f1
1108-Aubert-Dreyden <dreydenaubert@gmail.com>
2024-12-10 01:37:09 +0000

    dreyden madlib code

---
+++ madlibs.c
@@ -0,0 +1,119 @@
+// dreyden aubert
+// 11/26/24
+// madlibs
+
+#include <stdio.h>
+#define FN "madlib1.txt"
+#define AD "an adjective"
+#define NO "a noun"
+#define VE "a verb"
+#define NUM_LINES 200
+#define STRCAP 200
+ 
+int store(FILE* p, int rows, int cols, char str[][STRCAP]);
+int stringLength(char str[]);
+void prompt(char str[][STRCAP], int rows, int cols);
+int stringCompare(char str1[], char compared[]);
+void equivalency(char str[][STRCAP], int row, char strb[], char strc[]);
+int space(char str[][STRCAP], int row, int cols);
+
+int main(int argc, char *argv[]){
+	int numlines = 0;
+	char madlibStr[NUM_LINES][STRCAP]; // rows arbitrarily large
+
+	
+	FILE* fp = fopen(FN, "r"); // file io for reading madlib into program
+	if(fp == NULL){
+		printf("Can't open %s.\n", FN);
+		return 0;
+	}
+	else{
+		if(argc > 1){//no arguments passed makes argc = 1, so >1 to prevent segfault
+			char name[9] = {"filename"};//argument
+			if(stringCompare(name, argv[1]) == 0){
+				printf("\nThe madlib file name is: %s.\n", FN);
+			}
+			else{
+				printf("\nInvalid argument.\n");
+			}
+		}//might put into separate function
+		int rows = store(fp, NUM_LINES, STRCAP, madlibStr);
+		//printf("\n%d\n", rows);//test
+	 	prompt(madlibStr, rows, STRCAP);
+	 	for (int i = 0; i < rows; i++){
+	 		printf("%s", madlibStr[i]);
+	 		if(space(madlibStr, i, STRCAP) == 0){
+	 			printf(" ");
+	 		}
+	 	}
+	 	printf("\n");
+		fclose(fp);
+		return 0;
+	}
+}
+
+int store(FILE* p, int rows, int cols, char str[][STRCAP]){
+	int numRows = 0;
+	for(int i = 0; i < rows; i++){
+		fgets(str[i], STRCAP, p);
+		str[i][stringLength(str[i]) - 1] = '\0'; // removes endline character fgets inserts
+		numRows = i;
+		if(stringLength(str[i]) == 0){
+			break; // exits loop once blank line encountered
+		}
+	}
+	return numRows;
+}
+int stringLength(char str[]){
+	int count = 0;
+	for(int i = 0; str[i] != '\0'; i++){
+		count++;
+	}
+	return count;
+} // my (dreyden) length fxn from project 6
+void prompt(char str[][STRCAP], int rows, int cols){
+	char stra[2] = {'A', '\0'}, strn[2] = {'N', '\0'}, strv[2] = {'V', '\0'};
+	for(int i = 0; i < rows; i++){
+		equivalency(str, i, stra, AD);
+		equivalency(str, i, strn, NO);
+		equivalency(str, i, strv, VE);
+	}
+	
+}
+int stringCompare(char str1[], char compared[]){
+	int diff = 0;
+	for(int i = 0; str1[i] != '\0'; i++){
+		if(str1[i] > compared[i] || stringLength(str1) > stringLength(compared)){
+			diff = 1;
+		}
+		if(str1[i] < compared[i] || stringLength(str1) < stringLength(compared)){
+			diff = -1;
+		}
+	}
+
+	return diff;
+} // dreyden compare fxn (project 6)
+void equivalency(char str[][STRCAP], int row, char strb[], char strc[]){
+	int val = stringCompare(str[row], strb);
+		switch(val){
+			case 0:
+				printf("Enter %s: ", strc);
+				scanf("%s", str[row]);
+				break;
+		}
+}
+int space(char str[][STRCAP], int row, int cols){
+	int val = 0;
+	char first = str[row][0];
+	switch(first){
+		case '.':
+		case ',':
+		case '?':
+		case '!':
+		//case: '"':
+			val = 1;
+			break;
+	}
+	return val;
+}
+
